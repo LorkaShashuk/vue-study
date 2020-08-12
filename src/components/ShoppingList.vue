d40cfbffcf586ab59813618c9da325d40f035c59
LorkaShashuk <larisa.shashuk@gmail.com>
2020-08-13 00:06:42 +0300

    first commit

---
+++ src/components/ShoppingList.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="shoppingList">
+    <ul>
+      <li
+        v-for="fructs in fruitsList "
+        :key="fructs.id"
+        v-on:click=" onClick"
+      >Фрукт: {{fructs.title}} кількість: {{fructs.caunt}}</li>
+    </ul>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  name: "Data",
+  data() {
+    return {
+      fruitsList: [
+        {
+          id: 0,
+          title: "Aplle",
+          caunt: 10
+        },
+        {
+          id: 1,
+          title: "Orange",
+          caunt: 15
+        },
+        {
+          id: 2,
+          title: "Banan",
+          caunt: 20
+        }
+      ]
+    };
+  },
+  methods: {
+    onClick: function(event) {
+      event.target.style.color = "red";
+    }
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+</style>
+
