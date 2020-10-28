84b5df687ac1452f09b51a5043a650b7d3f3f216
Larysa <larisa.shashuk@gmail.com>
2020-10-28 13:38:46 +0200

    new

---
+++ src/components/Task2.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h1>{{ text }}</h1>
+
+    <ul>
+      <li
+        v-for="(item, index) in shopingList"
+        :key="index"
+      >{{index + 1}}. {{item.title}}: {{item.counte}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Text2",
+  props: {
+    text: String
+  },
+  data() {
+    return {
+      shopingList: [
+        {
+          id: 0,
+          title: "apple",
+          counte: 10
+        },
+        {
+          id: 1,
+          title: "kiwi",
+          counte: 20
+        },
+        {
+          id: 2,
+          title: "banana",
+          counte: 15
+        },
+        {
+          id: 3,
+          title: "peach",
+          counte: 10
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  color: red;
+}
+</style>
